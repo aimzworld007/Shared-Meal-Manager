ac9d9c8905f0d716570fa1e9cbe73702ccee0c38
Ainul islam <aimctgbd@gmail.com>
2025-10-16 20:56:01 +0400

    Update logo URL to new image

--- assets/logo.ts
+++ assets/logo.ts
@@ -2,4 +2,4 @@
  * @file assets/logo.ts
  * @summary Exports the application logo URL.
  */
-export const logoUrl = "https://i.ibb.co/3k04c6g/shared-meal-manager-logo.png";
+export const logoUrl = "https://i.ibb.co/ycwhj9tt/logo.jpg";
